8b3cdcb2358857ee7a8ad19f727adaaa43fa872e
y.aben <yerassyl.aben@intapp.com>
2025-02-09 22:16:47 +0100

    implementing data pipelines, reading csv and cleaning null values

---
+++ dags/execute_python_pipeline.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+
+from airflow import DAG
+
+from airflow.operators.python_operator import PythonOperator
+
+
+default_args = {
+    'owner': 'yerassyl',
+}
+
+def read_csv_file():
+    df = pd.read_csv('/Users/erasylabenov/airflow/dags/datasets/insurance.csv')
+    print(df)
+    return df.to_json()
+
+def remove_null_values(**kwargs):
+    ti = kwargs['ti']
+    df = pd.read_json(ti.xcom_pull(task_ids='read_csv_file'))
+    df = df.dropna()
+    print(df)
+    return df.to_json()
+
+
+with DAG(
+    dag_id='cross_task_communication',
+    description='Cross task communication',
+    default_args=default_args,
+    start_date=days_ago(1),
+    schedule_interval='@once',
+    tags=['python', 'transform', 'pipeline'],
+) as dag: 
+
+    read_csv_file = PythonOperator(
+        task_id='read_csv_file',
+        python_callable=read_csv_file,
+    )
+
+    remove_null_values = PythonOperator(
+        task_id='remove_null_values',
+        python_callable=remove_null_values,
+    )
+
+read_csv_file >> remove_null_values
+    
