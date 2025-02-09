9f3043a063ab6510badffc855cc1a00e7bcd2de2
y.aben <yerassyl.aben@intapp.com>
2025-02-09 21:36:57 +0100

    adding args

--- dags/execute_multiple_tasks.py
+++ dags/execute_multiple_tasks.py
@@ -11,17 +11,12 @@ default_args = {
 }
 
 #Create 4 tasks with simple prints
-def task1():
-    print('Task 1 is running')
+def task1(name: str):
+    print(f"Task 1 is running with name: {name}")
 
-def task2():
-    print('Task 2 is running')
+def task2(name: str, age: int):
+    print(f"Task 2 is running with name: {name} and age: {age}")
 
-def task3():
-    print('Task 3 is running')
-
-def task4():
-    print('Task 4 is running')
 
 
 
@@ -34,25 +29,17 @@ with DAG(
     tags=['dependencies', 'python'],
 ) as dag:
     task1 = PythonOperator(
-        task_id='task1',
+        task_id='task1_with_name',
         python_callable=task1,
+        op_kwargs={'name': 'Yerassyl'},
     )
 
     task2 = PythonOperator(
-        task_id='task2',
+        task_id='task2_with_name_and_age',
         python_callable=task2,
-    )
-
-    task3 = PythonOperator(
-        task_id='task3',
-        python_callable=task3,
-    )
-
-    task4 = PythonOperator(
-        task_id='task4',
-        python_callable=task4,
+        op_kwargs={'name': 'Yerassyl', 'age': 20},
     )
 
 
-task1 >> [task2, task3]
-[task2, task3] >> task4 
+
+task1 >> task2 
